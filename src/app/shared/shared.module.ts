8c62b21bfac31bff5b14b3703f04bcc34c984f8b
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2021-10-24 14:01:26 +0530

    Testimonial component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TestimonialComponent
   ],
   imports: [
     CommonModule,
+    IvyCarouselModule,
     RouterModule
   ],
   exports: [
